be5b744050c900762474271c69343577f4802231
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-09-18 20:48:54 +0200

    ?

--- workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/ReportActivity.java
+++ workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/ReportActivity.java
@@ -11,5 +11,6 @@ public class ReportActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_activity);
+
     }
 }
