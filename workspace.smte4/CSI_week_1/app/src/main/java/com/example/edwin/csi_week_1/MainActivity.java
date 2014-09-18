f5ecd8daf2f6f4f565fb2ebf121e1c8bcfb78ee7
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-09-18 20:49:08 +0200

    OnClickListener toegevoegd

--- workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/MainActivity.java
+++ workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.edwin.csi_week_1;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +15,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bReport = (Button) findViewById(R.id.btnReport);
+        bReport.setOnClickListener(myhandler1);
     }
+
+    View.OnClickListener myhandler1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, ReportActivity.class);
+            startActivity(intent);
+        }
+    };
 
 
     @Override
