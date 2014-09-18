89c43819275576f9108b5901a4efc71d98916ab1
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-09-18 20:21:31 +0200

    ReportActivity ingevuld

---
+++ workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/ReportActivity.java
@@ -0,0 +1,15 @@
+package com.example.edwin.csi_week_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Edwin on 18-9-2014.
+ */
+public class ReportActivity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_activity);
+    }
+}
