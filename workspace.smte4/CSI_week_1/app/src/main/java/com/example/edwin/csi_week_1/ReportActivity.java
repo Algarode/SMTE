0df66d1f094233b732308ec0504d6ae9297f0413
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-09-18 20:54:03 +0200

    ReportActivity afgemaakt

--- workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/ReportActivity.java
+++ workspace.smte4/CSI_week_1/app/src/main/java/com/example/edwin/csi_week_1/ReportActivity.java
@@ -1,7 +1,8 @@
 package com.example.edwin.csi_week_1;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Edwin on 18-9-2014.
@@ -11,6 +12,14 @@ public class ReportActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_activity);
-
+        Button btnBack = (Button) findViewById(btnBack);
+        btnBack.setOnClickListener(myhandler1);
     }
+
+    View.OnClickListener myhandler1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
 }
