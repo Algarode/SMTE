cd3a2fe77e3dc030a3b00a5aa4016598b0a0cf27
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-09-19 11:46:32 +0200

    CSI week 2

---
+++ workspace.smte4/CSI_week_2/app/src/main/java/com/example/edwin/csi_week_2/ReportActivity.java
@@ -0,0 +1,25 @@
+package com.example.edwin.csi_week_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Edwin on 18-9-2014.
+ */
+public class ReportActivity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_activity);
+        Button btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(myhandler1);
+    }
+
+    View.OnClickListener myhandler1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+}
