2ab2845e2db30ac906bfd2793e74f3abcea9dbea
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-10-01 20:39:05 +0200

    Listview werkend

--- workspace.smte4/CSI_week_2/app/src/main/java/com/example/edwin/csi_week_2/CriminalsListActivity.java
+++ workspace.smte4/CSI_week_2/app/src/main/java/com/example/edwin/csi_week_2/CriminalsListActivity.java
@@ -1,7 +1,12 @@
 package com.example.edwin.csi_week_2;
 
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Edwin on 1-10-2014.
@@ -10,7 +15,22 @@ public class CriminalsListActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.criminalslist);
 
+        ListView lv = (ListView) findViewById(R.layout.lvCriminals);
+        final String[] criminals = getResources().getStringArray(R.array.names);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, criminals));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = criminals[position];
+
+                Intent intent = new Intent(CriminalsListActivity.this, MainActivity.class);
+                intent.putExtra("Name", name);
+                startActivity(intent);
+            }
+        });
     }
 
 }
