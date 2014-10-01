8166f8f33ad3439153916c6c74f099ab34a759aa
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-10-01 20:10:57 +0200

    CriminalsList gemaakt

---
+++ workspace.smte4/CSI_week_2/app/src/main/java/com/example/edwin/csi_week_2/CriminalsListActivity.java
@@ -0,0 +1,16 @@
+package com.example.edwin.csi_week_2;
+
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Created by Edwin on 1-10-2014.
+ */
+public class CriminalsListActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+}
