aa4983db9a80fd0961647c002afd14b5b88e2e72
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-10-03 10:37:49 +0200

    SMS sender

---
+++ workspace.smte4/SmsOpdracht/app/src/main/java/com/example/edwin/smsopdracht/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.edwin.smsopdracht;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText telnummer = (EditText) findViewById(R.id.etTelnummer);
+        EditText bericht = (EditText) findViewById(R.id.etBericht);
+        Button stuur = (Button) findViewById(R.id.btnStuur);
+
+        final String sTelnummer = telnummer.getText().toString();
+        final String sBericht = bericht.getText().toString();
+
+        stuur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(sTelnummer, null, sBericht, null, null);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
