ca37ae5568aca46082a11dca592939aa066b965e
Edwin Lambregts <edwin.lambregts0@gmail.com>
2014-11-02 23:41:59 +0100

    Reactie klasse

---
+++ workspace.smte4/Koffiescanner/Koffiescanner/Reactie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koffiescanner
+{
+    class Reactie
+    {
+        public string Naam { get; set; }
+        public string Commentaar { get; set; }
+
+        public Reactie(string naam, string commentaar)
+        {
+            this.Naam = naam;
+            this.Commentaar = commentaar;
+        }
+    }
+}
